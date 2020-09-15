75796d3cd93da3508b0774d309b0620da38c8ef7
JW Nicholson <john.nicholson812@gmail.com>
2020-09-15 12:37:17 -0400

    can print stories as objects to page

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,14 +3,17 @@ import { getStory } from '../services/hnApi';
 
 
 const Story = ({storyId}) => {
+    //set story state with an empty object
+    const [story, setStory] = useState({});
+
 
     useEffect(() => {
-
+        getStory(storyId).then(data => data && data.url && setStory(data));
     },[]);
 
     return (
         <div>
-            <p>This is {storyId}</p>
+            <p>{JSON.stringify(story)}</p>
         </div>
     )
 }
